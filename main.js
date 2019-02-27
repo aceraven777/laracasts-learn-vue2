3d5c8e8caa8e5ea88762b5b3b4b7c11f3a275229
Aristheo Lacdao <aceraven777@gmail.com>
2019-02-27 10:27:01 +0800

    Episode 12: Component Communication Example 2: Event Dispatcher

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+window.Event = new class {
+    constructor() {
+        this.vue = new Vue();
+    }
+
+    fire(event, data = null) {
+        this.vue.$emit(event, data);
+    }
+
+    listen(event, callback) {
+        this.vue.$on(event, callback);
+    }
+};
+
 Vue.component('coupon', {
     template: '<input placeholder="Enter your coupon code" @blur="onCouponApplied" />',
 
     methods: {
         onCouponApplied() {
-            this.$emit('applied');
+            Event.fire('applied');
         },
     },
 });
@@ -15,9 +29,9 @@ new Vue({
         couponApplied: false,
     },
 
-    methods: {
-        onCouponApplied() {
+    created() {
+        Event.listen('applied', () => {
             this.couponApplied = true;
-        },
+        });
     },
 });
