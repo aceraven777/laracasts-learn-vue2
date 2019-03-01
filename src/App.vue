3c5da8736022b859a7c2edea9999234ef41d1ea6
Aristheo Lacdao <aceraven777@gmail.com>
2019-03-01 11:23:12 +0800

    Episode 17: Hot Module Replacement

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,19 @@
 <template>
   <div id="app">
-    <message>Hello There</message>
-    <message>Hello There Again</message>
+    <counter></counter>
   </div>
 </template>
 
 <script>
-import Message from './components/Message.vue';
+import Counter from './components/Counter.vue';
 
 export default {
   name: 'app',
-  components: { Message },
+  components: { Counter },
   data () {
     return {
-      
+      msg: 'Welcome to Your Vue.js App'
     }
   }
 }
 </script>
-
-<style>
-
-</style>
