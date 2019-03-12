053b24c1916e3193ff6411ec9a5c6e7ec526176e
Aristheo Lacdao <aceraven777@gmail.com>
2019-03-12 11:06:13 +0800

    Episode 24: Shared State 101

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,14 +3,23 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="/css/app.css" />
-
+        
         <title>My App</title>
     </head>
     <body>
-        <div id="app">
-            <notification></notification>
+        <div id="one">
+            <h1>
+                @{{ shared.user.name }}
+            </h1>
         </div>
+
+        <div id="two">
+            <h1>
+                @{{ shared.user.name }}
+            </h1>
+        </div>
+        
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
         <script src="/js/app.js"></script>
     </body>
 </html>
