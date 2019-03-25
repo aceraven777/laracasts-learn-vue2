76f43d91805fcc771d7ac950762b2115bd8e10bd
Aristheo Lacdao <aceraven777@gmail.com>
2019-03-25 11:21:38 +0800

    Episode 33 > Scoped Slots

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+        <div id="app">
+            <menu-list :items="['one', 'two', 'three']">
+                <template slot="menu-item" scope="props">
+                    <div>
+                        <em v-text="props.item"></em>
+                    </div>
+                </template>
+            </menu-list>
+        </div>
+
+        <script src="/js/app.js"></script>
+    </body>
+</html>
